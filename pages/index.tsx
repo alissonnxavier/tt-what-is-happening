05ea1adb325d1ed6a6b38679670f3d8b0ccaf567
Alisson <alissonalves29@gmail.com>
2023-08-19 12:21:24 -0300

    started creating the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Form from "@/components/Form";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <Header label='Home' showBackArrou={false}/>
+      <Header label='Home' showBackArrou={false} />
+      <Form placeholder="What's happenig" />
     </>
   )
 }
